e8685a3f236faea4c7ab828acf7a5c943a25480b
seokjin0414 <sars21@hanmail.net>
2021-09-14 15:17:47 +0900

    java_study 첫번째

---
+++ src/study_inf/ex1_1/Hello.java
@@ -0,0 +1,15 @@
+package study_inf.ex1_1;
+
+public class Hello {
+	
+	static int num;
+	public static void main(String[] args) {
+		
+		int anotherNum = 5;
+		num = 2;
+		
+		System.out.println(num + anotherNum);
+		System.out.println("Num: " + num);
+		System.out.println("AnotherNum: " + anotherNum);
+	}
+}
