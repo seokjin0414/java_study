5490abefe27bc07e97b56d200f7828e23bee3e29
seokjin0414 <sars21@hanmail.net>
2021-09-30 13:14:35 +0900

    2_2 end

---
+++ src/inflearn_java01/c02/ex2/Code10.java
@@ -0,0 +1,5 @@
+package inflearn_java01.c02.ex2;
+
+public class Code10 {
+
+}
--- src/inflearn_java01/c02/ex2/OrthoLine.java
+++ src/inflearn_java01/c02/ex2/OrthoLine.java
@@ -10,11 +10,11 @@ public class OrthoLine {
 		if (p.x > q.x || p.x == q.x && p.y > q.y)
 			swap();
 	}
-	
+
 	public void swap() {
 		MyPoint2 tmp = u;
 		u = v;
-		v = tmp;	
+		v = tmp;
 	}
 
 	public OrthoLine(int x1, int y1, int x2, int y2) {
@@ -23,18 +23,14 @@ public class OrthoLine {
 	}
 
 	public boolean isVertical() {
-		return u.x == u.y;
+		return (u.x == v.x);
 	}
 
 	public boolean intersects(OrthoLine other) {
 		if (isVertical() && other.isVertical()) {
-			
-			
-			
+			return (other.u.x < u.x && other.v.x > u.x && u.y < other.u.y && v.y > other.u.y);
 		} else if (!isVertical() && other.isVertical()) {
-
-			
-			
+			return (other.u.y < u.y && other.v.y > u.y && u.x < other.u.x && v.x > other.u.x);
 		} else
 			return false;
 	}
